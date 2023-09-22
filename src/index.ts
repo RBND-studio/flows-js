ffb985717223ee97a6c57e41a0255ce666a68be9
Vojtěch Vidra <vojtechvidra@gmail.com>
2023-09-22 07:14:30 +0200

    feat: umd and dts output

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-const a = 'a';
-console.log(a);
+import { computePosition } from '@floating-ui/dom';
+
+type Options = unknown;
+
+const render = (): void => {
+  computePosition(document.body, document.body).catch(() => null);
+};
+
+export const init = (options: Options): void => {
+  // eslint-disable-next-line no-console -- this is a demo
+  console.log('You initialized FlowsJS', options);
+  render();
+};
--- tsup.config.ts
+++ tsup.config.ts
@@ -4,4 +4,7 @@ export default defineConfig({
   entry: ['src/index.ts'],
   clean: true,
   format: ['cjs', 'esm', 'iife'],
+  globalName: 'FlowsJS',
+  minify: true,
+  dts: true,
 });
