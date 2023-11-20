99dd2c3054b2f1a6b60722e9872d348961582fe7
Vojtěch Vidra <vojtechvidra@gmail.com>
2023-11-20 09:22:38 +0100

    feat: get flows from cloud

--- src/cloud/api.ts
+++ src/cloud/api.ts
@@ -1,18 +1,20 @@
-const f = (url: string, { body, method }: { method: string; body: unknown }): Promise<Response> =>
+import type { Flow } from "../types";
+
+const f = <T>(
+  url: string,
+  { body, method }: { method?: string; body?: unknown } = {},
+): Promise<T> =>
   fetch(url, {
     method,
     headers: {
       "Content-Type": "application/json",
     },
     body: body ? JSON.stringify(body) : undefined,
-  });
+  }).then((res) => res.json() as Promise<T>);
 
-export const sendEvent = ({
-  baseUrl,
-  body,
-}: {
-  baseUrl: string;
-  body: {
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type -- not needed
+export const api = (baseUrl: string) => ({
+  sendEvent: (body: {
     eventTime: string;
     type: string;
     userHash?: string;
@@ -21,9 +23,10 @@ export const sendEvent = ({
     stepIndex?: string;
     stepHash?: string;
     flowHash: string;
-  };
-}): Promise<Response> =>
-  f(`${baseUrl}/events`, {
-    method: "POST",
-    body,
-  });
+  }): Promise<void> =>
+    f(`${baseUrl}/events`, {
+      method: "POST",
+      body,
+    }),
+  getFlows: (projectId: string): Promise<Flow[]> => f(`${baseUrl}/flows?projectId=${projectId}`),
+});
--- src/cloud/index.ts
+++ src/cloud/index.ts
@@ -1,36 +1,41 @@
 import { init as flowsInit } from "../index";
 import type { FlowsCloudOptions } from "../types";
 import { hash } from "../utils";
-import { sendEvent } from "./api";
+import { api } from "./api";
 
 export * from "../index";
 
-export const init = (options: FlowsCloudOptions): void => {
+export const init = async (options: FlowsCloudOptions): Promise<void> => {
   const API_URL =
     process.env.NODE_ENV === "production"
       ? "https://api.flows-cloud.com"
       : "https://api.stage.flows-cloud.com";
 
+  const flows = await api(API_URL)
+    .getFlows(options.projectId)
+    .catch((err) => {
+      // eslint-disable-next-line no-console -- useful for debugging
+      console.error(`Failed to fetch flows from cloud with projectId: ${options.projectId}`, err);
+    });
+
   flowsInit({
     ...options,
+    flows: [...(options.flows ?? []), ...(flows || [])],
     tracking: (event) => {
       options.tracking?.(event);
 
       const { flowHash, flowId, type, projectId, stepIndex, stepHash, userId } = event;
 
       void (async () =>
-        sendEvent({
-          baseUrl: API_URL,
-          body: {
-            eventTime: new Date().toISOString(),
-            flowHash,
-            flowId,
-            projectId,
-            type,
-            stepHash,
-            stepIndex: stepIndex?.toString(),
-            userHash: userId ? await hash(userId) : undefined,
-          },
+        api(API_URL).sendEvent({
+          eventTime: new Date().toISOString(),
+          flowHash,
+          flowId,
+          projectId,
+          type,
+          stepHash,
+          stepIndex: stepIndex?.toString(),
+          userHash: userId ? await hash(userId) : undefined,
         }))();
     },
   });
