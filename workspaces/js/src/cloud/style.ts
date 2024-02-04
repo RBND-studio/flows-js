212d7539529d2d95febb51eb231db8b1a57917a1
Vojtěch Vidra <vojtechvidra@gmail.com>
2024-02-04 14:12:55 +0100

    feat: cloud css version

--- workspaces/js/src/cloud/style.ts
+++ workspaces/js/src/cloud/style.ts
@@ -1,9 +1,11 @@
+import { version } from "../../package.json";
+
 export const loadStyle = ({ apiUrl, projectId }: { apiUrl: string; projectId: string }): void => {
   const styleEl =
     (document.querySelector("#flows-cloud-styles") as HTMLLinkElement | undefined) ??
     document.createElement("link");
   styleEl.id = "flows-cloud-styles";
   styleEl.rel = "stylesheet";
-  styleEl.href = `${apiUrl}/sdk/css?projectId=${projectId}`;
+  styleEl.href = `${apiUrl}/sdk/css?projectId=${projectId}&v=${version}`;
   document.head.appendChild(styleEl);
 };
