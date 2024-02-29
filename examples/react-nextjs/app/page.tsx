5cb467b257ab7a7f0e50c53ec218886c695647a4
Vojtěch Vidra <vojtechvidra@gmail.com>
2024-02-29 10:53:04 +0100

    feat: vanilla js example

--- examples/react-nextjs/app/page.tsx
+++ examples/react-nextjs/app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <>
-      <h1>React Next.js example</h1>
+      <h1>Flows - React Next.js example</h1>
       <button id="start-local">Start local flow</button>
       <button id="start-cloud">Start flow from cloud</button>
     </>
