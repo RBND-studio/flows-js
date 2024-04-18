25bee615a29a25a670b8ff1a2a706e65edaf2a72
OPesicka <ondrejpesicka@gmail.com>
2024-04-18 17:26:59 +0200

    feat: add og images to examples
    resolves #52

--- examples/react-nextjs/app/layout.tsx
+++ examples/react-nextjs/app/layout.tsx
@@ -9,6 +9,24 @@ export const metadata: Metadata = {
   title: "Flows - React Next.js example",
   description:
     "Flows lets you build any onboarding you want. Guide users, increase feature adoption, and improve revenue.",
+  openGraph: {
+    type: "website",
+    title: "Flows - React Next.js example",
+    description:
+      "Flows lets you build any onboarding you want. Guide users, increase feature adoption, and improve revenue.",
+    images: "/og.png",
+    url: "/",
+    locale: "en_US",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Flows - React Next.js example",
+    description:
+      "Flows lets you build any onboarding you want. Guide users, increase feature adoption, and improve revenue.",
+    images: "/og.png",
+    creator: "@flows_sh",
+  },
+  keywords: ["flows", "onboarding", "product adoption", "user onboarding", "user adoption"],
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
