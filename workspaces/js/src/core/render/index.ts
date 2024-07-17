fb2f9b1f0eb957fb4b104992a3121461851c6267
Vojtěch Vidra <vojtechvidra@gmail.com>
2024-07-17 10:13:49 +0200

    feat: banner step type (#156)
    * feat: banner step type
    
    * 0.1.1-canary.2
    
    * feat: split banner screen and card padding
    
    ---------
    
    Co-authored-by: flowsbotapp[bot] <170794745+flowsbotapp[bot]@users.noreply.github.com>

--- workspaces/js/src/core/render/index.ts
+++ workspaces/js/src/core/render/index.ts
@@ -1,3 +1,4 @@
 export * from "./render";
 export { renderTooltipElement, updateTooltip } from "./render-tooltip";
 export { renderModalElement } from "./render-modal";
+export { renderBannerElement } from "./render-banner";
--- workspaces/js/src/lib/step-type.ts
+++ workspaces/js/src/lib/step-type.ts
@@ -1,6 +1,9 @@
-import type { FlowStep, FlowTooltipStep, FlowModalStep } from "../types";
+import type { FlowStep, FlowTooltipStep, FlowModalStep, FlowBannerStep } from "../types";
 
 export const isTooltipStep = (step: FlowStep): step is FlowTooltipStep =>
   "targetElement" in step && Boolean(step.targetElement);
+export const isBannerStep = (step: FlowStep): step is FlowBannerStep =>
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition -- remove this ignore after we add type to other step types
+  "type" in step && step.type === "banner";
 export const isModalStep = (step: FlowStep): step is FlowModalStep =>
-  !isTooltipStep(step) && ("title" in step || "body" in step);
+  !isTooltipStep(step) && ("title" in step || "body" in step) && !isBannerStep(step);
