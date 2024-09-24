d090db11939c71c6ef46c67181d298e15fe4d5e7
Vojtěch Vidra <vojtechvidra@gmail.com>
2024-09-24 10:13:18 +0200

    feat: block trigger

--- workspaces/react/src/block.tsx
+++ workspaces/react/src/block.tsx
@@ -1,4 +1,4 @@
-import { type FC } from "react";
+import { useMemo, type FC } from "react";
 import { type Block as IBlock } from "./types";
 import { useFlowsContext } from "./flows-context";
 
@@ -9,15 +9,61 @@ interface Props {
 export const Block: FC<Props> = ({ block }) => {
   const { components, transition } = useFlowsContext();
 
-  const Component = components[block.type];
-  if (!Component) return null;
-  const methods = block.exitNodes.reduce(
-    (acc, exitNode) => ({
-      ...acc,
-      [exitNode]: () => transition({ exitNode, blockId: block.id }),
-    }),
-    {},
+  const methods = useMemo(
+    () =>
+      block.exitNodes.reduce(
+        (acc, exitNode) => ({
+          ...acc,
+          [exitNode]: () => transition({ exitNode, blockId: block.id }),
+        }),
+        {},
+      ),
+    [block.exitNodes, block.id, transition],
   );
 
-  return <Component key={block.id} {...block.data} {...methods} />;
+  const data = useMemo(() => {
+    const processData = ({
+      properties,
+      parentKey,
+    }: {
+      properties: Record<string, unknown>;
+      parentKey?: string;
+    }): Record<string, unknown> => {
+      const _data = { ...properties };
+
+      // Recursively process nested objects
+      Object.entries(properties).forEach(([key, value]) => {
+        if (Array.isArray(value)) {
+          _data[key] = value.map((item: Record<string, unknown>, index) => {
+            if (typeof item === "object") {
+              return processData({
+                properties: item,
+                parentKey: [parentKey, key, index].filter((x) => x !== undefined).join("."),
+              });
+            }
+            return item;
+          });
+        }
+      });
+
+      // Add exit node methods
+      delete _data.f__exit_nodes;
+      (properties.f__exit_nodes as string[] | undefined)?.forEach((exitNode) => {
+        _data[exitNode] = () =>
+          transition({
+            exitNode: [parentKey, exitNode].filter((x) => x !== undefined).join("."),
+            blockId: block.id,
+          });
+      });
+
+      return _data;
+    };
+
+    return processData({ properties: block.data });
+  }, [block.data, block.id, transition]);
+
+  const Component = components[block.type];
+  if (!Component) return null;
+
+  return <Component key={block.id} {...data} {...methods} />;
 };
