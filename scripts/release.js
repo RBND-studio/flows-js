25d7891e59490031432428f8c0db0678904d6504
Vojtěch Vidra <vojtechvidra@gmail.com>
2024-11-09 07:35:51 +0100

    build: release with package option

--- scripts/release.js
+++ scripts/release.js
@@ -1,29 +1,33 @@
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 
-const canary = process.argv.includes("--canary");
-const patch = process.argv.includes("--patch");
-const minor = process.argv.includes("--minor");
-const major = process.argv.includes("--major");
+const argv = require("minimist")(process.argv.slice(2));
+const { release, package } = argv;
 
-if (!patch && !minor && !major && !canary)
-  throw new Error("You must specify a release type: --patch, --minor, --major or --canary");
+const allowedReleases = ["canary", "patch", "minor", "major"];
+
+if (!allowedReleases.includes(release))
+  throw new Error(`You must specify a release type (--release=...): ${allowedReleases.join(", ")}`);
+
+const allowedPackages = ["js", "react"];
+if (!allowedPackages.includes(package))
+  throw new Error(`You must specify a package (--package=...): ${allowedPackages.join(", ")}`);
 
 const main = async () => {
-  if (canary) await exec("pnpm js version prerelease --preid=canary");
-  else await exec(`pnpm js version ${patch ? "patch" : minor ? "minor" : "major"}`);
+  if (release === "canary") await exec(`pnpm ${package} version prerelease --preid=canary`);
+  else await exec(`pnpm ${package} version ${release}`);
 
-  const currentVersion = require("../workspaces/js/package.json").version;
+  const currentVersion = require(`../workspaces/${package}/package.json`).version;
   await exec(`git commit -am "${currentVersion}"`);
   const gitTagName = `v${currentVersion}`;
   await exec(`git tag -a ${gitTagName} -m '${gitTagName}'`);
   await exec("git push --no-verify");
   await exec(`git push --no-verify --tags`);
 
-  await exec("pnpm js build");
-  await exec("cp README.md workspaces/js");
+  await exec(`pnpm ${package} build`);
+  await exec(`cp README.md workspaces/${package}`);
   await exec(
-    `pnpm js publish --access=public --provenance --no-git-checks ${canary ? "--tag=canary" : ""}`,
+    `pnpm ${package} publish --access=public --provenance --no-git-checks ${release === "canary" ? "--tag=canary" : ""}`,
   );
 };
 
