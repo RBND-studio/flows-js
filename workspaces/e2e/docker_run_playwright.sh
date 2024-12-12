afeaf1e610b26cfc9be932082845c37c3300c62b
Vojtěch Vidra <vojtechvidra@gmail.com>
2024-12-12 13:14:20 +0100

    chore: update playwright in docker script

--- workspaces/e2e/docker_run_playwright.sh
+++ workspaces/e2e/docker_run_playwright.sh
@@ -6,5 +6,5 @@ docker run --rm --ipc=host \
   --mount type=bind,src=./../../,dst=/app \
   --mount type=volume,dst=/app/node_modules \
   --mount type=volume,dst=/app/.pnpm-store \
-  mcr.microsoft.com/playwright:v1.48.1-jammy \
+  mcr.microsoft.com/playwright:v1.49.1-jammy \
   $1
